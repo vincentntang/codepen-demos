7ab395e0b703836d60efc7145ca406f9c3b8cf00
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-12-23 12:10:05 -0500

    initialize a black circle

--- amazon-canvas-demo/script.js
+++ amazon-canvas-demo/script.js
@@ -1,5 +1,21 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.querySelector("canvas");
 
-const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+
+var x = 5;
+var y = 5;
+var radius = 30;
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  ctx.strokeStyle = "black";
+  ctx.stroke();
+}
+
+animate();

59c5de4332801d0aca30415adbf75572c1fdac94
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-12-23 12:34:14 -0500

    spawn circles from chris canvas courses

--- amazon-canvas-demo/script.js
+++ amazon-canvas-demo/script.js
@@ -3,19 +3,58 @@ var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const ctx = canvas.getContext("2d");
+var c = canvas.getContext("2d");
 
-var x = 5;
-var y = 5;
-var radius = 30;
+// Constructor Function (object blueprint)
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
 
-function animate() {
-  requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, innerWidth, innerHeight);
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-  ctx.strokeStyle = "black";
-  ctx.stroke();
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "green";
+    c.stroke();
+    c.fillStyle = "lightgrey";
+    c.fill();
+  };
+
+  this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  };
 }
 
+// var circle = new Circle(200,200, 3,3, 30);
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+  var radius = 30;
+  var x = Math.random() * (innerWidth - radius * 2) + radius;
+  var y = Math.random() * (innerHeight - radius * 2) + radius; // + radius prevents circles spawning on edge
+  var dx = (Math.random() - 0.5) * 8;
+  var dy = (Math.random() - 0.5) * 8;
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+function animate() {
+  requestAnimationFrame(animate); // recurisvely run
+  c.clearRect(0, 0, innerWidth, innerHeight); // erases previously drawn content
+
+  for (var i = 0; i < circleArray.length; i++) {
+    // circleArray[i].draw();
+    circleArray[i].update();
+  }
+}
 animate();
